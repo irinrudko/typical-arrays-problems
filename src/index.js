98c2ee26b29cf5a8d8546835a3b7c8a0232a9919
Iryna Rudzko <irynarudzko@MacBook-Air-Iryna.local>
2021-03-10 21:02:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else return Math.max(...array);
+  // let maxValue = array[0];
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] >= maxValue) {
+  //     maxValue = array[i];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
